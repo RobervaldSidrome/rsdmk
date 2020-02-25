9875ba527e2679550735fdf65eb62d227f609c83
Robervald da Silva Sidrome <rsidrome@gmail.com>
2020-02-24 23:19:59 -0400

    configuraçao no package e server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const nomeApp = process.env.rsdmk;
 const app = express();
- 
-app.use(express.static(`$/dist/$`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`$/dist/$/index.html`));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
- 
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
